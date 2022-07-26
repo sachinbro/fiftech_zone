af0a18ca77675585742b3ebbbb8132f5ecced5d1
Sachin Shilwal <34100453+sachinbro@users.noreply.github.com>
2022-07-26 18:50:00 +0545

    animation template implemented: using radial progress bar

--- components/sections/home-page-sections/ProjectInfo.vue
+++ components/sections/home-page-sections/ProjectInfo.vue
@@ -1,3 +1,38 @@
+<script>
+    import RadialProgressBar from 'vue-radial-progress'
+
+        export default {
+        data () {
+            return {
+            completedSteps: 10,
+            totalSteps: 100,
+            totalProject: 0,
+            customerSatisfaction: 0,
+            }
+        },
+
+        components: {
+            RadialProgressBar
+        },
+        methods: {
+            initiateAnimation(){
+                console.log("initialAnimation");
+                if(this.completedSteps > 10){
+                    this.completedSteps = 10;
+                    console.log("reset", this.completedSteps);
+                    return 
+                }
+                
+                this.completedSteps = 50;
+                this.totalSteps = 100;
+                this.totalProject = 50;
+            },
+            initiateAnimationCustomer(){
+                this.customerSatisfaction = 100;
+            }
+        }
+        }
+</script>
 <template>
     <v-row class="projectinfo pa-4">
         <v-col cols="1"></v-col>
@@ -7,13 +42,34 @@
             <div>no more, no less.</div>
             <v-row class="text-subtitle-1">
                 <v-col>
-                    <div>
-                        
+                    <div @click="initiateAnimation()">
+                        <radial-progress-bar :diameter="150"
+                        innerStrokeColor=""
+                        startColor="white"
+                        stopColor="white"
+                       :completed-steps="completedSteps"
+                       :total-steps="totalSteps"
+                       class="progress-bar"
+                       >
+                    
+                    </radial-progress-bar>
                     </div>
-                    Completed 0 + projects
+                    Completed {{totalProject}} + projects
                 </v-col>
                 <v-col>
-                    0% customer satisfaction
+                     <div @click="initiateAnimationCustomer()">
+                        <radial-progress-bar :diameter="150"
+                        innerStrokeColor=""
+                        startColor="white"
+                        stopColor="white"
+                       :completed-steps="customerSatisfaction"
+                       total-steps="100"
+                       class="progress-bar"
+                       >
+                    
+                    </radial-progress-bar>
+                    </div>
+                    {{customerSatisfaction}}% customer satisfaction
                 </v-col>
             </v-row>
         </v-col>
@@ -26,5 +82,9 @@
 <style scoped>
     .projectinfo{
          background: transparent linear-gradient(10deg, #9B51E0 0%, #347FE2 76%, #0693E3 100%) 0% 0% no-repeat padding-box;
+    }
+    .progress-bar{
+        border-radius: 50%;
+        box-shadow: inset 0px 3px 15px #FFFFFF8C, 0px 3px 99px #00000052;
     }
 </style>
