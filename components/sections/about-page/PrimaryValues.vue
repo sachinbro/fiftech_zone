0db5ccb02784302a2a5a34b58a2ba610a41c2ec0
Sachin Shilwal <34100453+sachinbro@users.noreply.github.com>
2022-07-27 17:54:59 +0545

    timeline added

---
+++ components/sections/about-page/PrimaryValues.vue
@@ -0,0 +1,70 @@
+<script>
+    export default {
+        data(){
+            return {
+                primaryValues: [
+                    {
+                        title: 'Integrity',
+                        description: 'There are many variations of lorem duos lora spassages of Lorem Ipsum available internet tend to repeat predefined.',
+                        img: 'Image 6.png',
+                        
+                    },
+                     {
+                        title: 'Accountability',
+                        description: 'There are many variations of lorem duos lora spassages of Lorem Ipsum available internet tend to repeat predefined.',
+                        img: 'Image 7.png',
+                        
+                    },
+                    {
+                        title: 'Diligence',
+                        description: 'There are many variations of lorem duos lora spassages of Lorem Ipsum available internet tend to repeat predefined.',
+                        img: 'Image 8.png',
+                        
+                    },
+                    {
+                        title: 'Perseverance',
+                        description: 'There are many variations of lorem duos lora spassages of Lorem Ipsum available internet tend to repeat predefined.',
+                        img: 'Image 13.png',
+                    },
+                    {
+                        title: 'Discipline',
+                        description: 'There are many variations of lorem duos lora spassages of Lorem Ipsum available internet tend to repeat predefined.',
+                        img: 'Image 10.png',
+                    }
+                    
+                ]
+            }
+        }
+    }
+</script>
+<template>
+    <div class="mt-16" >
+        <p class="text-h6 text-center">FIVE PRIMARY VALUES OF FIFTECHZONE</p>
+        <p class="text-center text--disabled">There are many variations of lorem passages of Lorem Ipsum available, but <br> the majority have suffered. All the Lorem Ipsum generators.</p>
+        <v-row>
+            <v-col>
+
+            </v-col>
+            <v-col cols="6">
+                <v-timeline>
+                <v-timeline-item 
+                v-for="value in primaryValues"
+                :key="value.title"
+                small>
+                    <img :src="require(`../../../static/sections/Aboutpage/${value.img}`)" height="250px" alt="" >
+                    <template v-slot:opposite>
+                        <v-card elevation="0">
+                            <v-card-title>{{value.title}}</v-card-title>
+                            <v-card-text>
+                                {{value.description}}
+                            </v-card-text>
+                        </v-card>
+                    </template>
+                </v-timeline-item>
+            </v-timeline>
+            </v-col>
+            <v-col></v-col>
+        </v-row>
+        
+    </div>
+</template>
--- pages/about.vue
+++ pages/about.vue
@@ -1,15 +1,17 @@
 <script>
 import AboutIntro from '../components/sections/about-page/AboutIntro.vue';
 import VisionMission from '../components/sections/about-page/VisionMission.vue';
+import PrimaryValues from '../components/sections/about-page/PrimaryValues.vue';
 
 export default {
     name: "AboutPage",
-    components: { AboutIntro, VisionMission }
+    components: { AboutIntro, VisionMission, PrimaryValues }
 }
 </script>
 <template>
     <div>
        <AboutIntro />
        <VisionMission />
+       <PrimaryValues />
     </div>
 </template>
