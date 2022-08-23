01a87a2f0c45515604f2917080456e740ebd1e5f
Sachin Shilwal <34100453+sachinbro@users.noreply.github.com>
2022-08-23 12:11:38 +0545

    responnsiveness of careerpage fixed

--- components/sections/career-page/CareerIntro.vue
+++ components/sections/career-page/CareerIntro.vue
@@ -10,11 +10,11 @@
             <v-col class="mt-md-16" cols="4" sm="3"><img src="../../../static/sections/Careerpage/Image 47.png" height="300px" alt=""></v-col>
             <v-col cols="1"></v-col>
         </v-row>
-        <p class="text-h5 text-center font-weight-bold">Why join us?</p>
+        <p class="mt-10 mt-sm-0 text-h5 text-center font-weight-bold">Why join us?</p>
         <p class="text-center text-secondary">
             Sample text. Click to select the text box. Click again or double click to start editing the text.<br> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
         </p>
-        <v-row class="mt-16">
+        <v-row class="mt-16 d-none d-sm-flex">
             <v-col cols="1"></v-col>
             <v-col>
                 <div class="pa-2 rounded-circle" style="border: 1px solid rgba(112, 112, 112, 1); background-color:#E9D1FF;">
@@ -23,7 +23,7 @@
                     <v-avatar size="150" left><img src="../../../static/sections/Careerpage/Image 46.png" alt=""/></v-avatar>
                 </div>
                 </div>
-                
+
             </v-col>
             <v-col cols="1"  class="d-none d-sm-flex align-center justify"><div style="border: 1px solid rgba(112, 112, 112, 1); width: 100%; "></div></v-col>
             <v-col>
@@ -44,6 +44,35 @@
                 </div>
             </v-col>
             <v-col cols="1"></v-col>
+        </v-row>
+        <v-row class="d-flex d-sm-none">
+          <v-col>
+            <div class="pa-2 rounded-circle" style="border: 1px solid rgba(112, 112, 112, 1); background-color:#E9D1FF;">
+              <div class="pa-16 rounded-circle" style="border: 1px solid #070707; background-color: white;">
+                <p class="text-center font-weight-medium">Faster growing company</p>
+                <v-avatar size="170" class="ml-4" ><img src="../../../static/sections/Careerpage/Image 46.png" alt=""/></v-avatar>
+              </div>
+            </div>
+
+          </v-col>
+
+          <v-col>
+            <div class="pa-2 rounded-circle" style="border: 1px solid rgba(112, 112, 112, 1); background-color:#E9D1FF;">
+              <div class="pa-16 rounded-circle" style="border: 1px solid rgba(112, 112, 112, 1); background-color: white;">
+                <p class="text-center font-weight-medium">Great Collegues</p>
+                <v-avatar size="170" class="ml-4"><img src="../../../static/sections/Careerpage/Image 49.png" alt=""/></v-avatar>
+              </div>
+            </div>
+          </v-col>
+
+          <v-col>
+            <div class="pa-2 rounded-circle" style="border: 1px solid rgba(112, 112, 112, 1); background-color:#E9D1FF;">
+              <div class="pa-16 rounded-circle" style="border: 1px solid rgba(112, 112, 112, 1); background-color: white;">
+                <p class="text-center font-weight-medium">Take Charge</p>
+                <v-avatar size="170" class="ml-4"><img src="../../../static/sections/Careerpage/Image 46.png" alt=""/></v-avatar>
+              </div>
+            </div>
+          </v-col>
         </v-row>
     </v-container>
 </template>
